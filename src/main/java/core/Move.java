faa814496f23d7704bbabe46093c794f4fa28c43
NadineHuetter <nadine.huetter@fh-erfurt.de>
2023-06-17 22:37:31 +0200

    the Moves as a Struct (class)

---
+++ src/main/java/core/Move.java
@@ -0,0 +1,14 @@
+package core;
+
+public class Move {
+    private int startpoint;
+    private int endpoint;
+
+    public Move(int startpoint, int endpoint) {
+        this.startpoint = startpoint;
+        this.endpoint = endpoint;
+    }
+
+
+
+}
